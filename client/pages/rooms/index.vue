829676fd219b936700bd31d146717dfc792f486e
briancaffey <briancaffey2010@gmail.com>
2022-08-21 16:14:21 -0400

    add home link

--- client/pages/rooms/index.vue
+++ client/pages/rooms/index.vue
@@ -6,8 +6,13 @@
       <Room v-for="room in rooms" :room="room" :key="room" />
     </div>
 
-    <div class="flex justify-center pt-4">
+    <div class="flex justify-center p-2">
       <button class="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded" @click="fetchRooms">More</button>
+    </div>
+
+    <div class="flex justify-center p-2">
+    <nuxt-link class="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded justify-center" to="/">Home
+    </nuxt-link>
     </div>
   </div>
 </template>
